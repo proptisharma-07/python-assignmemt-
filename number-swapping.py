a86c514145ce100dc6847117db53bd85c55c2a14
proptisharma-07 <proptisharma@gmail.com>
2026-05-04 09:05:24 +0530

    02-conditional-statements.py

---
+++ number-swapping.py
@@ -0,0 +1,40 @@
+#Basic Variable Swapping
+# 1. Switch values of two integers
+# Input:  n1 = 20, 
+# n2 = 30 
+# Output:  n1 = 30 n2 = 20 
+
+n1=20
+n2=30
+temp = n1
+n1=n2
+n2 = temp
+print("n1 =", n1)
+print("n2 =", n2)
+
+#2. Switch values of two strings (characters) 
+# Input: char1 = "hello" 
+# char2 = "java" 
+# Output:  char1 = "java" char2 = "hello"
+
+char1 = "hello"
+char2 = "java"
+temp = "hello"
+char1 = char2
+char2 = temp
+print("char1 =", char1)
+print("char2 =", char2)
+
+# 3. Switch one string value with one integer value
+# Input:  n1 = 200,  
+# char2 = "java" 
+# Output:  n1 = "java" char2 = 200 
+
+
+n1 =200
+char2 = "java"
+temp = n1
+n1 = char2
+char2 = temp
+print("n1 =", n1)
+print("char2 =", char2)
